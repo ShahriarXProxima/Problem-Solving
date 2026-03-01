475b9470a13324174ff251aebd6ec2f1f9a60d2e
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-03-02 02:43:41 +0600

    03/26

---
+++ src/InsomniaCure.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class InsomniaCure {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int k = input.nextInt();
+        int l = input.nextInt();
+        int m = input.nextInt();
+        int n = input.nextInt();
+        int d = input.nextInt();
+
+        int damaged = 0;
+
+        for (int i = 1; i <= d; i++) {
+            if (i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0) {
+                damaged++;
+            }
+        }
+
+        System.out.print(damaged);
+    }
+}
+
