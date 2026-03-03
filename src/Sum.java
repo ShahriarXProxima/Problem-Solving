14020c147b629e01e57102268b8e736f6c4b4f9a
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-03-04 01:27:49 +0600

    03/26

--- src/Sum.java
+++ src/Sum.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Sum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+
+        while (test-- > 0) {
+            int a = input.nextInt();
+            int b = input.nextInt();
+            int c = input.nextInt();
+
+            if ((a == b + c) || (b == c + a) || (c == b + a)) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
 }
