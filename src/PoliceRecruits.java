22ae1b78896b9f6357f490ac305d6aa08d7b4002
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-03-05 02:07:08 +0600

    03/26

--- src/PoliceRecruits.java
+++ src/PoliceRecruits.java
@@ -1,3 +1,27 @@
-public class PoliceRecruits {
+import java.util.Scanner;
 
+public class PoliceRecruits {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int officers = 0;
+        int untreated = 0;
+
+        for (int i = 0; i < n; i++) {
+            int event = input.nextInt();
+
+            if (event > 0) {
+                officers += event;
+            } else {
+                if (officers > 0) {
+                    officers--;
+                } else {
+                    untreated++;
+                }
+            }
+        }
+
+        System.out.print(untreated);
+    }
 }
