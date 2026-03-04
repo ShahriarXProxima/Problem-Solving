e3d950427efc3e5dfa4d2562da57c631f0c492c7
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-03-04 22:49:53 +0600

    03/26

--- src/AmusingJoke.java
+++ src/AmusingJoke.java
@@ -1,2 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class AmusingJoke {
+    public static String sort(String str) {
+        char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+
+        return str = new String(chars);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String[] hostAndGuests = new String[3];
+        for (int i = 0; i < 3; i++) {
+            hostAndGuests[i] = input.nextLine();
+        }
+
+        String names = hostAndGuests[0] + hostAndGuests[1];
+        String pile = hostAndGuests[2];
+
+        names = sort(names);
+        pile = sort(pile);
+
+        if (names.equals(pile)) {
+            System.out.print("YES");
+        } else {
+            System.out.print("NO");
+        }
+    }
 }
