c9500ae40919540a3f6d31a9fd80d0f586c5c534
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-03-05 01:29:12 +0600

    03/26

--- src/TheNewYearMeetingFriends.java
+++ src/TheNewYearMeetingFriends.java
@@ -1,3 +1,18 @@
-public class TheNewYearMeetingFriends {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class TheNewYearMeetingFriends {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[] friends = new int[3];
+        for (int i = 0; i < 3; i++) {
+            friends[i] = input.nextInt();
+        }
+
+        Arrays.sort(friends);
+
+        int distance = (friends[2] - friends[1]) + (friends[1] - friends[0]);
+        System.out.print(distance);
+    }
 }
