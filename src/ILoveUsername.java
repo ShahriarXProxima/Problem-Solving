3ac11d8e88645bbe2035907d485925a0bd181004
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-03-07 23:10:41 +0600

    03/26

--- src/ILoveUsername.java
+++ src/ILoveUsername.java
@@ -1,5 +1,33 @@
-public class ILoveUsername {
- public   static void main(String[] args) {
+import java.util.Scanner;
 
+public class ILoveUsername {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int[] points = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            points[i] = input.nextInt();
+        }
+
+        int count = 0;
+        int maxPoint = points[0];
+        int minPoint = points[0];
+
+        if (points.length == 1) {
+            System.out.print(count);
+        } else {
+            for (int i = 0; i < n; i++) {
+                if (maxPoint < points[i]) {
+                    count++;
+                    maxPoint = points[i];
+                } else if (minPoint > points[i]) {
+                    count++;
+                    minPoint = points[i];
+                }
+            }
+            System.out.print(count);
+        }
     }
 }
