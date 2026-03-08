612ad22b4ab007f7f83990e6821420a62be83284
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-03-08 14:19:30 +0600

    03/26

---
+++ src/AgainTwentyFive.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class AgainTwentyFive {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        Long n = input.nextLong();
+        System.out.print("25");
+    }
+}
