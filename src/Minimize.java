9f3369bfa872e8aec7a2818a795fb5b01fcc31c4
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-03-17 15:56:32 +0600

    03/26

--- src/Minimize.java
+++ src/Minimize.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class Minimize {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        while (test-- > 0) {
+            int a = input.nextInt();
+            int b = input.nextInt();
+            System.out.println(b - a);
+        }
+    }
 
 }
