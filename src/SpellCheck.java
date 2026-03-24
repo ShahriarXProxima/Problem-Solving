549824b0947d6f059d6573182237264cb2ba4b58
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-03-25 02:50:27 +0600

    03/26

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class SpellCheck {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        int test = input.nextInt();
+        while (test-- > 0) {
+            int n = input.nextInt();
+            String name = input.next();
+
+            String tRemoved = name.replace("T","0");
+            char[] nameArray = tRemoved.toCharArray();
+            Arrays.sort(nameArray);
+            String str = new String(nameArray);
+
+            if (n == 5 && name.contains("T") && str.equals("0imru")){
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
     }
 }
