95f25b7b8e50a89b6ff4a557539de648c3ac9b03
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-03-25 23:09:25 +0600

    03/26

---
+++ src/MaximumGCD.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MaximumGCD {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        while (test-- > 0) {
+            int n = input.nextInt();
+            System.out.println(n / 2);
+        }
+    }
+}
