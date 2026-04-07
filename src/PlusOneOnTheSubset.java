84aa47ee4c6267b2debdc177840d9497558a5789
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-04-08 00:50:31 +0600

    04/26

--- src/PlusOneOnTheSubset.java
+++ src/PlusOneOnTheSubset.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class PlusOneOnTheSubset {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        while (test-- > 0) {
+            int n = input.nextInt();
+            int[] arr = new int[n];
+
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = input.nextInt();
+
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
+                if (arr[i] < min) {
+                    min = arr[i];
+                }
+            }
+
+            System.out.println(max - min);
+        }
+    }
 }
