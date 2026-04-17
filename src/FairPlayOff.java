ab35f9a0cca72ca2cc6314cffd4e4ae01a6c6ede
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-04-17 22:59:40 +0600

    04/26

---
+++ src/FairPlayOff.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class FairPlayOff {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        while(test-->0){
+
+        }
+    }
+}
