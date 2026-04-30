d774e7f1610b28b14ee80b1b4a77742b3b818f25
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-05-01 02:23:41 +0600

    04/26

--- src/Triple.java
+++ src/Triple.java
@@ -1,3 +1,34 @@
-public class Triple {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+public class Triple {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        while (test-- > 0) {
+            int n = input.nextInt();
+            int[] arr = new int[n];
+            Map<Integer, Integer> arrMap = new HashMap<>();
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = input.nextInt();
+                arrMap.put(arr[i], arrMap.getOrDefault(arr[i], 0) + 1);
+            }
+
+            boolean flag = false;
+            for (Map.Entry<Integer, Integer> entry : arrMap.entrySet()) {
+                if (entry.getValue() >= 3) {
+                    System.out.println(entry.getKey());
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag) {
+                System.out.println(-1);
+            }
+        }
+    }
 }
