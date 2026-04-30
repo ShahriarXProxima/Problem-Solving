bdcc14beb9027e4a622d38cef127e6c3ca4ed74d
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-04-30 14:27:35 +0600

    04/26

--- src/Desorting.java
+++ src/Desorting.java
@@ -5,9 +5,35 @@ public class Desorting {
         Scanner input = new Scanner(System.in);
 
         int test = input.nextInt();
-        while(test-->0){
+        while (test-- > 0) {
+            int n = input.nextInt();
+            long[] arr = new long[n + 1];
+            for (int i = 1; i <= n; i++) {
+                arr[i] = input.nextLong();
+            }
 
+            if (!isSorted(arr)) {
+                System.out.println(0);
+            } else {
+                long ans = Long.MAX_VALUE;
+                for (int i = 1; i <= n - 1; i++) {
+                    long d = arr[i] - arr[i + 1];
+                    long ops = (1 - d + 1) / 2;
+
+                    ans = Math.min(ans, ops);
+                }
+                System.out.println(ans);
+            }
 
         }
+    }
+
+    static boolean isSorted(long[] arr) {
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
