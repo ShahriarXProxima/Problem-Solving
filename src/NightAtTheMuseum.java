69cb8d9892e757a7d872fd74caf723f8d5d84cbd
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-05-09 03:31:08 +0600

    05/26

--- src/NightAtTheMuseum.java
+++ src/NightAtTheMuseum.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class NightAtTheMuseum {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        String str = input.next();
+        int rotation = 0;
+        char current = 'a';
+
+        for (int i = 0; i < str.length(); i++) {
+            char target = str.charAt(i);
+            int diff = Math.abs(target - current);
+            int moves = Math.min(diff, 26 - diff);
+            rotation +=moves;
+            current = target;
+        }
+
+        System.out.println(rotation);
     }
-
 }
