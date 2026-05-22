3b7c3af4b0a9ab9d7d5a2d2e46ea5673db84181c
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-05-22 21:05:53 +0600

    05/26

--- src/ClockConversion.java
+++ src/ClockConversion.java
@@ -1,3 +1,12 @@
+import java.util.Scanner;
+
 public class ClockConversion {
-    ps
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        while (test-->0){
+
+        }
+    }
 }
