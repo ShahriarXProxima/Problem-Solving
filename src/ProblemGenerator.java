f436cddbe2d9d05b52d0c010e4da895efebb22ae
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-05-22 19:42:37 +0600

    05/26

---
+++ src/ProblemGenerator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ProblemGenerator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        while (test-- > 0) {
+            int n = input.nextInt();
+            int round = input.nextInt();
+            String problem = input.next();
+
+            int[] count = new int[7];
+            for (int i = 0; i < n; i++) {
+                char ch = problem.charAt(i);
+                count[ch - 'A']++;
+            }
+
+            int totalProblemNeeded = 0;
+            for (int i = 0; i < count.length; i++) {
+                if (count[i] < round) {
+                    totalProblemNeeded += round - count[i];
+                }
+            }
+            System.out.println(totalProblemNeeded);
+        }
+    }
+}
