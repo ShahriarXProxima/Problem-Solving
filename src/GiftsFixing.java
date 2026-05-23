2d754159f39beca7b2789660240557f20242e5d8
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-05-23 18:39:31 +0600

    05/26

--- src/GiftsFixing.java
+++ src/GiftsFixing.java
@@ -1,3 +1,35 @@
-public class GiftsFixing {
+import java.util.Scanner;
 
+public class GiftsFixing {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        while (test-- > 0) {
+            long minCandy = Integer.MAX_VALUE;
+            long minORange = Integer.MAX_VALUE;
+
+            int n = input.nextInt();
+            long[] candy = new long[n];
+            long[] orange = new long[n];
+
+            for (int i = 0; i < n; i++) {
+                candy[i] = input.nextInt();
+                minCandy = Math.min(minCandy, candy[i]);
+            }
+            for (int i = 0; i < n; i++) {
+                orange[i] = input.nextInt();
+                minORange = Math.min(minORange, orange[i]);
+            }
+
+            long totalMoves = 0;
+            for (int i = 0; i < n; i++) {
+                long deltaCandy = candy[i] - minCandy;
+                long deltaOrange = orange[i] - minORange;
+
+                totalMoves += Math.max(deltaCandy, deltaOrange);
+            }
+            System.out.println(totalMoves);
+        }
+    }
 }
