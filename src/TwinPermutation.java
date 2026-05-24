e0ecd3b708066d715eec8872330c55524397f34d
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-05-24 14:46:31 +0600

    05/26

--- src/TwinPermutation.java
+++ src/TwinPermutation.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class TwinPermutation {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        int test = input.nextInt();
+        while (test-- > 0) {
+            int n = input.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = input.nextInt();
+            }
+
+            for (int i = 0; i < n; i++) {
+                System.out.print((n + 1 - arr[i]) + " ");
+            }
+            System.out.println();
+        }
     }
 }
