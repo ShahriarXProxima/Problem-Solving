a5ae59fa2aba6893d07adc908b50217f751e80cb
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-05-25 15:34:42 +0600

    05/26

--- src/CPlusEqualsTo.java
+++ src/CPlusEqualsTo.java
@@ -2,6 +2,24 @@ import java.util.Scanner;
 
 public class CPlusEqualsTo {
     public static void main(String[] args) {
-        Scanner in
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        while (test-- > 0) {
+            int a = input.nextInt();
+            int b = input.nextInt();
+            int n = input.nextInt();
+
+            int count = 0;
+            while (a <= n && b <= n) {
+                if (a < b) {
+                    a += b;
+                } else {
+                    b += a;
+                }
+                count++;
+            }
+            System.out.println(count);
+        }
     }
 }
