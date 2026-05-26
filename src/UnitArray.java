304756335a5c8d74a38dabd82668dd8da7615314
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-05-26 20:13:34 +0600

    05/26

---
+++ src/UnitArray.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class UnitArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        while(test-->0){
+            int n = input.nextInt();
+            int[] arr = new int[n];
+            for(int i =0; i<n; i++){
+                arr[i]= input.nextInt();
+            }
+
+
+
+
+        }
+    }
+}
