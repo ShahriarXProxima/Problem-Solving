4c274bb1f7d1a0b4e00ed58b6d0e5cc2668c3b0e
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-05-27 14:16:43 +0600

    05/26

---
+++ src/EhabAndGCD.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class EhabAndGCD {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        while (test-- > 0) {
+            int n = input.nextInt();
+
+            System.out.println(1 + " " + (n - 1));
+        }
+    }
+}
