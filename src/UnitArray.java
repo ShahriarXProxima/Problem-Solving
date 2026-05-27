bac80c660da6c1a32e2bb183c9d1270e21a20dce
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-05-27 14:08:14 +0600

    05/26

--- src/UnitArray.java
+++ src/UnitArray.java
@@ -5,16 +5,35 @@ public class UnitArray {
         Scanner input = new Scanner(System.in);
 
         int test = input.nextInt();
-        while(test-->0){
+        while (test-- > 0) {
             int n = input.nextInt();
             int[] arr = new int[n];
-            for(int i =0; i<n; i++){
-                arr[i]= input.nextInt();
+            for (int i = 0; i < n; i++) {
+                arr[i] = input.nextInt();
             }
 
+            int plusCount = 0;
+            int minusCount = 0;
+            for (int i = 0; i < n; i++) {
+                if (arr[i] < 0) {
+                    minusCount++;
+                } else {
+                    plusCount++;
+                }
+            }
 
+            int opt = 0;
+            while (minusCount > plusCount) {
+                minusCount--;
+                plusCount++;
+                opt++;
+            }
 
+            if (minusCount % 2 != 0) {
+                opt++;
+            }
 
+            System.out.println(opt);
         }
     }
 }
