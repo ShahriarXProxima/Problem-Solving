88805dd243411c9467eb645b4ea2a6056f8ab705
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-05-29 22:13:29 +0600

    05/26

---
+++ src/HardProblem.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HardProblem {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        while (test-->0){
+            int m = input.nextInt();
+            int a = input.nextInt();
+            int b = input.nextInt();
+            int c = input.nextInt();
+
+
+        }
+    }
+}
