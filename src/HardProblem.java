6158a3ba1fff0c0a6c62a8ba4beb738abc463d2e
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-05-29 22:50:22 +0600

    05/26

--- src/HardProblem.java
+++ src/HardProblem.java
@@ -5,13 +5,32 @@ public class HardProblem {
         Scanner input = new Scanner(System.in);
 
         int test = input.nextInt();
-        while (test-->0){
+        while (test-- > 0) {
             int m = input.nextInt();
             int a = input.nextInt();
             int b = input.nextInt();
             int c = input.nextInt();
 
+            int row1 = 0;
+            int row2 = 0;
 
+            if (m >= a) {
+                row1 += a;
+            } else {
+                row1 = m;
+            }
+
+            if (m >= b) {
+                row2 += b;
+            } else {
+                row2 = m;
+            }
+
+            int seatOccupied = row1 + row2;
+            int seatRemaining = (2 * m) - seatOccupied;
+
+            seatOccupied += Math.min(seatRemaining, c);
+            System.out.println(seatOccupied);
         }
     }
 }
