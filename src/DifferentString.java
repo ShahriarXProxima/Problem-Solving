27a16a405ec7edb0994aaa3a48c56eea626eba60
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-05-30 18:55:49 +0600

    05/26

--- src/DifferentString.java
+++ src/DifferentString.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -5,7 +6,6 @@ import java.util.Set;
 public class DifferentString {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         int test = input.nextInt();
         while (test-- > 0) {
             String str = input.next();
@@ -13,17 +13,23 @@ public class DifferentString {
             for (int i = 0; i < str.length(); i++) {
                 stringSet.add(str.charAt(i));
             }
-
             if (stringSet.size() == 1) {
                 System.out.println("NO");
                 continue;
             }
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
 
             System.out.println("YES");
-            for (int i = str.length() - 1; i >= 0; i--) {
-                System.out.print(str.charAt(i));
+            if (!sorted.equals(str)) {
+                System.out.println(sorted);
+            } else {
+                for (int i = chars.length - 1; i >= 0; i--) {
+                    System.out.print(chars[i]);
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
 }
