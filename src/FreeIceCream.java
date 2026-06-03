58118ee13f68619e38934e867a45c04b8556ea5f
Shahriar Tahmid <shahriarxproximalog1@gmail.com>
2026-06-03 23:06:17 +0600

    06/26

--- src/FreeIceCream.java
+++ src/FreeIceCream.java
@@ -7,14 +7,21 @@ public class FreeIceCream {
         int n = input.nextInt();
         int x = input.nextInt();
 
-        int distress =0;
-        for(int i =0; i<n; i++){
+        int distress = 0;
+        for (int i = 0; i < n; i++) {
             char sign = input.next().charAt(0);
             int iceCream = input.nextInt();
 
-            if(sign=='-' && ){
-
+            if (sign == '+') {
+                x += iceCream;
+                continue;
+            } else if (sign == '-' && x >= iceCream) {
+                x -= iceCream;
+                continue;
             }
+            distress++;
         }
+
+        System.out.println(x + " " + distress);
     }
 }
