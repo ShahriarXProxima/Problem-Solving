e0dc4daa12b342f1e7b4c9907a3a4e9ba0745aaa
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-06-03 09:17:43 +0600

    06/26

--- src/PrimaryTask.java
+++ src/PrimaryTask.java
@@ -7,6 +7,11 @@ public class PrimaryTask {
         int test = input.nextInt();
         while (test-- > 0) {
             String a = input.next();
+            if(a.length()<=1){
+                System.out.println("NO");
+                continue;
+            }
+
             if (a.charAt(0) == '1' && a.charAt(1) == '0' && a.length() > 2) {
                 String rest = a.substring(2);
                 if (rest.charAt(0) == '0') {
