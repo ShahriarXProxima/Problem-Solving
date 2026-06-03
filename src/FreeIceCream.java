8f589f7067248feb689fa5a27cb0640f68cc5497
Shahriar Tahmid <shahriarxproximalog1@gmail.com>
2026-06-03 22:58:09 +0600

    06/26

---
+++ src/FreeIceCream.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FreeIceCream {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int x = input.nextInt();
+
+        int distress =0;
+        for(int i =0; i<n; i++){
+            char sign = input.next().charAt(0);
+            int iceCream = input.nextInt();
+
+            if(sign=='-' && ){
+
+            }
+        }
+    }
+}
