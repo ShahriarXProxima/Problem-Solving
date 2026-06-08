03fe4247668640f90f89adf7f8597d012fdb0789
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-06-08 20:58:12 +0600

    06/26

---
+++ src/VusTheCossackAndAContest.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class VusTheCossackAndAContest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int m = input.nextInt();
+        int k = input.nextInt();
+
+        if (n <= m && n <= k) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
