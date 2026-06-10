b8b52dcfc798b88da1611d8ea1f4c09c8997b0cf
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-06-10 13:50:52 +0600

    06/26

--- src/BeautifulAverage.java
+++ src/BeautifulAverage.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class BeautifulAverage {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        int test = input.nextInt();
+        while (test-- > 0) {
+            int n = input.nextInt();
+            int max = Integer.MIN_VALUE;
+
+            for (int i = 0; i < n; i++) {
+                max = Math.max(max, input.nextInt());
+            }
+
+            System.out.println(max);
+        }
     }
 }
