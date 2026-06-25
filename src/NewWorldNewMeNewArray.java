dec798e9d02fa8b5c47364056ddaa023510a0151
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-06-26 02:18:18 +0600

    06/26

--- src/NewWorldNewMeNewArray.java
+++ src/NewWorldNewMeNewArray.java
@@ -5,11 +5,20 @@ public class NewWorldNewMeNewArray {
         Scanner input = new Scanner(System.in);
 
         int test = input.nextInt();
-        while (test-->0){
+        while (test-- > 0) {
             int n = input.nextInt();
             int k = input.nextInt();
             int p = input.nextInt();
 
+            int abs_k = Math.abs(k);
+
+            if (abs_k > n * p) {
+                System.out.println(-1);
+                continue;
+            }
+
+            int ans = (abs_k + p - 1) / p;
+            System.out.println(ans);
 
         }
     }
