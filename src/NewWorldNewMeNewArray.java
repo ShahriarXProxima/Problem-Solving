39ba48611cd8359a862722dbfd9aeaf1aa3dbf13
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-06-26 02:05:20 +0600

    06/26

---
+++ src/NewWorldNewMeNewArray.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class NewWorldNewMeNewArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        while (test-->0){
+            int n = input.nextInt();
+            int k = input.nextInt();
+            int p = input.nextInt();
+
+
+        }
+    }
+}
