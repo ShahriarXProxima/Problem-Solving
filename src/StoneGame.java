7e6a908d1ef43b906fb926fa78d1a6236bd2c8be
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-06-27 01:23:56 +0600

    06/26

---
+++ src/StoneGame.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class StoneGame {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        while(test-->0){
+
+        }
+    }
+}
