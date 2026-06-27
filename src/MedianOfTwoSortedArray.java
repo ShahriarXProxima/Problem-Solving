85a126d75ce76b7554285aac60d5d6b93c51939e
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-06-28 02:06:20 +0600

    06/26 (LeetCode)

---
+++ src/MedianOfTwoSortedArray.java
@@ -0,0 +1,17 @@
+public class MedianOfTwoSortedArray {
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] array = new int[nums1.length + nums2.length];
+
+
+
+    }
+}
+
+public static void main(String[] args) {
+    int[] arr1 = {1, 3};
+    int[] arr2 = {2};
+
+    double median = findMedianSortedArrays(arr1, arr2);
+    System.out.println(median);
+}
+}
