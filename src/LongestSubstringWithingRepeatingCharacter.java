d46aee452899faed5f1f3299fa9f1850d9573532
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-07-04 14:21:37 +0600

    07/26

--- src/LongestSubstringWithingRepeatingCharacter.java
+++ src/LongestSubstringWithingRepeatingCharacter.java
@@ -1,6 +1,25 @@
 public class LongestSubstringWithingRepeatingCharacter {
-    public static void main(String[] args) {
+    public int lengthOfLongestSubstring(String s) {
+        int[] charMap = new int[128];
+        for (int i = 0; i < 128; i++) {
+            charMap[i] = -1;
+        }
 
+        int maxLength = 0;
+        int left = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char currentChar = s.charAt(right);
+
+            if (charMap[currentChar] >= left) {
+                left = charMap[currentChar] + 1;
+            }
+
+            charMap[currentChar] = right;
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
     }
 }
 
