96a88aa7cf7ae92e25919ef3a4ff149cce2597e6
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-07-11 23:08:36 +0600

    07/26

--- src/WhoIsOpposite.java
+++ src/WhoIsOpposite.java
@@ -5,7 +5,23 @@ public class WhoIsOpposite {
         Scanner input = new Scanner(System.in);
 
         int test = input.nextInt();
-        while (test-->0){
+        while (test-- > 0) {
+            int a = input.nextInt();
+            int b = input.nextInt();
+            int c = input.nextInt();
+
+            int half_n = Math.abs(a - b);
+            int n = 2 * half_n;
+
+            if (a > n || b > n || c > n) {
+                System.out.println(-1);
+            } else {
+                if (c <= half_n) {
+                    System.out.println(c + half_n);
+                } else {
+                    System.out.println(c - half_n);
+                }
+            }
 
         }
     }
