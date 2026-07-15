778b2af75cc71438cf04b6e5525d5fae3c9a93b6
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-07-15 20:22:13 +0600

    07/26

---
+++ src/JumpGame.java
@@ -0,0 +1,18 @@
+public class JumpGame {
+    public static boolean jumpGame(int[] nums) {
+        int farthest = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > farthest) {
+                return false;
+            }
+
+            farthest = Math.max(farthest, i + nums[i]);
+
+            if (farthest >= nums.length - 1) {
+                return true;
+            }
+        }
+        return true;
+    }
+}
