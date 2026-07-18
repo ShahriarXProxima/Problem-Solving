ac9c40c504d469466d82ce77fee7b49700311260
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-07-19 03:59:52 +0600

    07/26

--- src/ZeroSum.java
+++ src/ZeroSum.java
@@ -7,11 +7,22 @@ public class ZeroSum {
         int test = input.nextInt();
         while (test-- > 0) {
             int n = input.nextInt();
-            int sum = 0;
-            for(int i = 1; i<=n; i++){
+            int minus = 0, plus = 0;
 
+            for (int i = 1; i <= n; i++) {
+                int num = input.nextInt();
+                if (num == 1) {
+                    plus++;
+                } else {
+                    minus++;
+                }
             }
 
+            if (n % 2 == 0 && (plus < n && minus < n)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
         }
     }
 }
