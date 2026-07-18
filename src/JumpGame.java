59e520b6d97f557d131b51308115916e086e367b
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-07-18 20:54:17 +0600

    07/26

--- src/JumpGame.java
+++ src/JumpGame.java
@@ -12,10 +12,6 @@ public class JumpGame {
             if (farthest >= nums.length - 1) {
                 return true;
             }
-
-
-
-
         }
         return true;
     }
---
+++ src/ZeroSum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ZeroSum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        while (test-- > 0) {
+            int n = input.nextInt();
+            int sum = 0;
+            for(int i = 1; i<=n; i++){
+
+            }
+
+        }
+    }
+}
