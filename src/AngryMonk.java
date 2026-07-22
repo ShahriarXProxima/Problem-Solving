17837efd139abcb37f8d5283d894cb00ab5102d2
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-07-22 09:33:25 +0600

    07/26

---
+++ src/AngryMonk.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AngryMonk {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        while (test-- > 0) {
+
+
+        }
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
--- src/WayTooLongWords.java
+++ src/WayTooLongWords.java
@@ -2,19 +2,25 @@ import java.util.Scanner;
 
 public class WayTooLongWords {
     public static void main(String[] args) {
-        String str;
         Scanner input = new Scanner(System.in);
-        int test = Integer.parseInt(input.nextLine());
 
+        int test = input.nextInt();
         while (test-- > 0) {
-            str = input.nextLine();
-            if (str.length() > 10) {
-                System.out.print("" + (str.charAt(0)) + (str.length() - 2) + (str.charAt(str.length() - 1)));
-            } else {
-                System.out.print(str);
-            }
-            System.out.println();
+            String str = input.next();
+            String s = word(str);
 
+            System.out.println(s);
         }
     }
+
+
+    public static String word(String str) {
+        if (str.length() > 10) {
+            return ("" + (str.charAt(0)) + (str.length() - 2) + (str.charAt(str.length() - 1)));
+        } else {
+            return (str);
+        }
+    }
+
+
 }
