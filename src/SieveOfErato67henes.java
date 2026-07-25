77f3a3c997fc5a0ba3cb77c8ba9f8dbc362f8e89
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-07-25 13:57:52 +0600

    07/26

---
+++ src/SieveOfErato67henes.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class SieveOfErato67henes {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        while (test-- > 0) {
+            int n = input.nextInt();
+            HashSet<Integer> set = new HashSet<>();
+
+            for (int i = 1; i <= n; i++) {
+                set.add(input.nextInt());
+            }
+
+            if (set.contains(67) && set.contains(1)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
