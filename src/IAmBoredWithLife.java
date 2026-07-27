8a82872cbe9d0c37e0bbfbad640fd55147e78959
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-07-27 23:24:41 +0600

    07/26

--- src/IAmBoredWithLife.java
+++ src/IAmBoredWithLife.java
@@ -1,3 +1,22 @@
-public class IAmBoredWithLife {
+import java.util.Scanner;
 
+public class IAmBoredWithLife {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt(), b = input.nextInt();
+        System.out.println(getGcd(getFact(a), getFact(b)));
+    }
+
+    public static int getGcd(int a, int b) {
+        return b == 0 ? a : getGcd(b, a % b);
+    }
+
+    public static int getFact(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        return n * getFact(n - 1);
+    }
 }
