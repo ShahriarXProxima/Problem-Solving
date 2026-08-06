139c003c4ae33191b9f6fb384ebd234b98980b74
shahriarxproxima <shahriarxproximalog1@gmail.com>
2026-08-06 14:48:51 +0600

    08/26

---
+++ src/DrTC.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DrTC {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test  = input.nextInt();
+        while (test-->0){
+            int n = input.nextInt();
+            String num = input.next();
+
+            
+
+        }
+    }
+}
