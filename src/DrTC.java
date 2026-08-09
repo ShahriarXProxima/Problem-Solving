a1e92be282ab5333bcb136f56f3f15f640c0e9a8
shahriarxproxima <shahriarxproximalog1@gmail.com>
2026-08-10 02:21:37 +0600

    08/26

--- src/DrTC.java
+++ src/DrTC.java
@@ -4,13 +4,20 @@ public class DrTC {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int test  = input.nextInt();
-        while (test-->0){
+        int test = input.nextInt();
+        while (test-- > 0) {
             int n = input.nextInt();
             String num = input.next();
 
-            
+            int oneCount = 0;
+            for (char ch : num.toCharArray()) {
+                if (ch == '1') {
+                    oneCount++;
+                }
+            }
 
+            int totalOnes = oneCount * (n - 1) + (n - oneCount);
+            System.out.println(totalOnes);
         }
     }
 }
