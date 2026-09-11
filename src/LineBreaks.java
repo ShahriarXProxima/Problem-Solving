df8bbe38ded8862e4adc46f6b52160a6a4852c5c
shahriarxproxima <shahriarxproximalog1@gmail.com>
2026-09-11 14:55:25 +0600

    09/26

--- src/LineBreaks.java
+++ src/LineBreaks.java
@@ -5,8 +5,23 @@ public class LineBreaks {
         Scanner input = new Scanner(System.in);
 
         int test = input.nextInt();
-        while (test-->0){
+        while (test-- > 0) {
+            int n = input.nextInt();
+            int m = input.nextInt();
 
+            int current_len = 0;
+            boolean stop = false;
+            int x = 0;
+            while (n-- > 0) {
+                String word = input.next();
+                if (!stop && current_len + word.length() <= m) {
+                    current_len += word.length();
+                    x++;
+                    continue;
+                }
+                stop = true;
+            }
+            System.out.println(x);
         }
     }
 }
