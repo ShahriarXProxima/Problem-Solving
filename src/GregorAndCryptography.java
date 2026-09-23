4dbcab2140e39f25219356372b8638e1102e60c9
shahriarxproxima <shahriarxproximalog1@gmail.com>
2026-09-23 19:20:53 +0600

    09/26

---
+++ src/GregorAndCryptography.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class GregorAndCryptography {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        while (test-- > 0) {
+            int p = input.nextInt();
+            if (p == 5) {
+                System.out.println(2 + " " + 4);
+                continue;
+            }
+            int a = 2;
+            int b = p / 2;
+
+            System.out.println(a + " " + b);
+        }
+    }
+}
