29efa639f12e3bc9f8f09608cd98352968b3d56f
shahriarxproxima <shahriarxproximalog1@gmail.com>
2026-09-24 00:27:12 +0600

    09/26

--- src/BlackslexAndPassword.java
+++ src/BlackslexAndPassword.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class BlackslexAndPassword {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        while (test-- > 0) {
+            int k = input.nextInt();
+            int x = input.nextInt();
+
+            System.out.println(k * x + 1);
+        }
 
     }
 }
